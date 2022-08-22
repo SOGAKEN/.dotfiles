7a22c9417c42b8be60090f77988677bce8dcf645
SOGAKEN <58077058+SOGAKEN@users.noreply.github.com>
2022-08-22 19:26:41 +0900

    tmux追加

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     # export LSCOLORS=Gxfxcxdxbxegedabagacad
 end
-# starship init fish | source
+starship init fish | source
