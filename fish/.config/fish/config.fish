a49f4da2acca98595a511d26ac665b0e6d460503
SOGAKEN <58077058+SOGAKEN@users.noreply.github.com>
2022-08-30 19:41:26 +0900

    add 20220830

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     # export LSCOLORS=Gxfxcxdxbxegedabagacad
 end
-starship init fish | source
+# starship init fish | source
+
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+
+
