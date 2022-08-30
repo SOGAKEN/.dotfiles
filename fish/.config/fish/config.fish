2fc07e1df7b5dae12fcebe8159e3b8e994238611
SOGAKEN <58077058+SOGAKEN@users.noreply.github.com>
2022-08-30 23:14:59 +0900

    add fzf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,17 +12,20 @@ alias ll "exa -l -g --icons"
 alias lla "exa -la"
 alias g git
 
+set -U FZF_LEGACY_KEYBINDINGS 0
+set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
+
 #fisherパッケージoh-my-fish/plugin-pecoの設定
-function fish_user_key_bindings
-  bind \cr peco_select_history # Bind for peco select history to Ctrl+R
-  bind \cf peco_change_directory # Bind for peco change directory to Ctrl+F
+# function fish_user_key_bindings
+#  bind \cr peco_select_history # Bind for peco select history to Ctrl+R
+#  bind \cf peco_change_directory # Bind for peco change directory to Ctrl+F
 
   # vim-like
-  bind \cl forward-char
+#  bind \cl forward-char
 
   # prevent iterm2 from closing when typing Ctrl-D (EOF)
-  bind \cd delete-char
-end
+##   bind \cd delete-char
+# end
 
 
 
