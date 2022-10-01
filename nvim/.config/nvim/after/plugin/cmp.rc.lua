f20c4041ae56c2d6b96bb982cdcbf58cccebf91e
SOGAKEN <sogaken0919@gmail.com>
2022-10-02 03:07:24 +0900

    auto complite

--- nvim/.config/nvim/after/plugin/cmp.rc.lua
+++ nvim/.config/nvim/after/plugin/cmp.rc.lua
@@ -1,37 +1,32 @@
-local status, cmp = pcall(require, "cmp")
-if (not status) then return end
-local lspkind = require 'lspkind'
-
-cmp.setup({
+local cmp = require'cmp'
+cmp.setup {
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
+      require'luasnip'.lsp_expand(args.body)
+    end
   },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true
-    }),
-  }),
+
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lsp_signature_help'},
+    {name = 'path'},
+    {name = 'buffer'},
+    {name = 'nvim_lua'},
+    {name = 'luasnip'},
+    {name = 'cmdline'},
+    {name = 'git'},
   }),
-  formatting = {
-    format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
-  }
-})
+}
 
-vim.cmd [[
-  set completeopt=menuone,noinsert,noselect
-  highlight! default link CmpItemKind CmpItemMenuDefault
-]]
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
--- " Use <Tab> and <S-Tab> to navigate through popup menu
--- inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
--- inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
+local lspconfig = require('lspconfig')
+require('mason-lspconfig').setup_handlers {
+  function(server_name)
+    lspconfig[server_name].setup {
+      capabilities = capabilities,
+    }
+  end,
+}
